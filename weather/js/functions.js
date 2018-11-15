fcfac8cf3eabaf1fed89bf7c582c58c12beb8c02
Christopher Leavitt <christopherleavitt@205-185-99-12.directcom.com>
2018-11-14 17:06:33 -0700

    changed some stuff

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -3,25 +3,23 @@
 ************************************* */
 // Calculate the Windchill
 function buildWC(speed, temp) {
+ const feelTemp = document.getElementById('feelTemp');
 
-}
-const feelTemp = document.getElementById('feelTemp');
-// Compute the windchill
-let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-console.log(wc);
-// Round the answer down to integer
-wc = Math.floor(wc);
-// If chill is greater than temp, return the temp
-wc = (wc > temp)?temp:wc;
-<p id="feel">Feels Like <span id="feelTemp">??</span>°F</p>
-// Display the windchill
-console.log(wc);
-feelTemp.innerHTML = wc;
-<p id="feelTemp">??</p>
-// Display the windchill
-console.log(wc);
-wc = 'Feels like ' + wc + '&deg;F';
-feelTemp.innerHTML = wc;
+ // Compute the windchill
+ let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+ console.log(wc);
+
+ // Round the answer down to integer
+ wc = Math.floor(wc);
+
+ // If chill is greater than temp, return the temp
+ wc = (wc > temp)?temp:wc;
+
+ // Display the windchill
+ console.log(wc);
+ // wc = 'Feels like '+wc+'°F';
+ feelTemp.innerHTML = wc;
+ }
 
 /*console.log('My javascript is being read.');
 var color;
