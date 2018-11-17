87d5595258949c7264c84901ca1551e94755c71a
Christopher Leavitt <christopherleavitt@205-185-99-12.directcom.com>
2018-11-17 13:16:51 -0700

    changes

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -2,7 +2,7 @@
  *  Weather Site JavaScript Functions
  ************************************* */
 // Calculate the Windchill
-function buildWC(speed, temp) {
+ƒtion buildWC(speed, temp) {
     const feelTemp = document.getElementById('feelTemp');
 
     // Compute the windchill
