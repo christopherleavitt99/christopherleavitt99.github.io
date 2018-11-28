a4dbe44f268d87ace81ac5ebac4b26d8554ccd26
Christopher Leavitt <christopherleavitt@Christophers-MacBook-Pro.local>
2018-11-28 13:17:22 -0700

    added stuff

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -7,10 +7,21 @@ getGeoLocation();
 
 
 //Gets latitude and longitude of current location
-function getGeoLocation(){
+function getGeoLocation() {
     const status = document.getElementById('status');
     status.innerHTML = 'Getting Location...';
-    if (navigator.geolocation){
-        
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
+            // Combine the values
+            const LOCALE = lat + "," + long;
+            console.log('Lat and Long are: ${LOCALE}.');
+
+            //Call getData function, send LOCALE
+            getComputedStyle(LOCALE);
+        })
+    } else{
+        status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
     }
 }
