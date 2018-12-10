eb71983128df9df50a92c2bcf1c99409b463b383
Christopher Leavitt <christopherleavitt@Christophers-MacBook-Pro.local>
2018-12-10 12:14:20 -0700

    added stuff

--- weather/js/search.js
+++ weather/js/search.js
@@ -38,7 +38,10 @@ function processJSON(searchValue) {
 
 
 function getSearchResults(event) {
-    
+   //return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 const searchResults = document.getElementById("searchResults")
 searchResults.addEventListener("click", getSearchResults);  // do stuff
+function getLocationByKey(locations) {
+   // return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
