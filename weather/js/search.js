6ef40af41dcdbc126e295ecb650ae27b0b6ccecb
Christopher Leavitt <christopherleavitt@Christophers-MacBook-Pro.local>
2018-12-10 12:18:14 -0700

    moved something

--- weather/js/search.js
+++ weather/js/search.js
@@ -40,8 +40,8 @@ function processJSON(searchValue) {
 function getSearchResults(event) {
    //return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
-const searchResults = document.getElementById("searchResults")
-searchResults.addEventListener("click", getSearchResults);  // do stuff
 function getLocationByKey(locations) {
    // return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
+const searchResults = document.getElementById("searchResults")
+searchResults.addEventListener("click", getSearchResults);  // do stuff
